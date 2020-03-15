0f5e03d6f3b893fa56ba229c5509ebb7ecf81905
bogdanistvan <bogdanistvan0924@gmail.com>
2020-03-15 14:21:09 +0100

    project created

---
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
