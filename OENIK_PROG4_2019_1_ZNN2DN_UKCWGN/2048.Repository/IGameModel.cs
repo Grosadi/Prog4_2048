a0596c6472ec7fe41b663e30fb03f08d882dd5e4
bogdanistvan <bogdanistvan0924@gmail.com>
2020-04-05 13:05:29 +0200

    IgameModel Interface had been edited

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/IGameModel.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/IGameModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="IGameModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace _2048.Repository
 {
-    interface IGameModel
+    using _2048.Repository.Seged;
+
+    /// <summary>
+    /// Define the class Gamemodel.
+    /// </summary>
+    public interface IGameModel
     {
+        /// <summary>
+        /// Gets or sets the board of the game.
+        /// </summary>
+        Tile[,] Board { get; set; }
+
+        /// <summary>
+        /// Gets or sets the score.
+        /// </summary>
+        int Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets highest score of all time.
+        /// </summary>
+        int Highest { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether if game is over.
+        /// </summary>
+        bool GameOver { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether if the player wins the game.
+        /// </summary>
+        bool Gamewin { get; set; }
+
     }
 }
