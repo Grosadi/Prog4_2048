45865b93a9e0cfe5fbb337e7a438946bd4746b0d
bogdanistvan <bogdanistvan0924@gmail.com>
2020-04-05 12:10:24 +0200

    Tile class added to Seged folder

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/Seged/Tile.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/Seged/Tile.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2048.Repository
+namespace _2048.Repository.Seged
 {
-    public class Class1
+    class Tile
     {
     }
 }
