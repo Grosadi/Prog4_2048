d02c944d867918fa8f01385fa570b372c63d072c
grosadi <groska.adam98@gmail.com>
2020-04-13 19:26:59 +0200

    GameRenderer class added

---
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/GameRenderer.cs
@@ -0,0 +1,15 @@
+﻿using _2048.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
+{
+   public class GameRenderer
+    {
+        GameModel model;
+
+    }
+}
