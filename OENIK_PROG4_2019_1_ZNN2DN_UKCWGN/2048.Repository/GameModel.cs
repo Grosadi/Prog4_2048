043e438baa37f991798896d18be5f9158e0f943f
grosadi <groska.adam98@gmail.com>
2020-04-13 19:27:41 +0200

    Working on GameModel

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/GameModel.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/2048.Repository/GameModel.cs
@@ -1,64 +1,68 @@
-﻿using _2048.Repository.Seged;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="GameModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace _2048.Repository
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using _2048.Repository.Seged;
+
     /// <summary>
     /// GameModel.
     /// </summary>
     public class GameModel : IGameModel
     {
-        IRepository repo;
+        private IRepository repo;
 
         /// <summary>
         /// Gets or sets the actual size of the board.
         /// </summary>
-        public int Gamesize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Gamesize { get; set; }
 
         /// <summary>
         /// Gets or sets the size of the board. Board[Gamesize, Gamesize].
         /// </summary>
-        public Tile[,] Board { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Tile[,] Board { get; set; }
 
         /// <summary>
         /// Gets or sets the actual score.
         /// </summary>
-        public int Score { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Score { get; set; }
 
         /// <summary>
         /// Gets or sets the highest Tile value.
         /// </summary>
-        public int Highest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Highest { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether if game is over.
         /// </summary>
-        public bool GameOver { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool GameOver { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether if the player wins the game.
         /// </summary>
-        public bool Gamewin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Gamewin { get; set; }
 
         /// <summary>
         /// Gets or sets the timer, to massure time.
         /// </summary>
-        public double DeltaTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double DeltaTime { get; set; }
 
         /// <summary>
         /// Gets or sets the duration of the match. Possibilities: 2min, 5 min, 10min, endless.
         /// </summary>
-        public int Matchtime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Matchtime { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether checkaviable attribution.
         /// </summary>
-        public bool CheckAvailableMoves { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool CheckAvailableMoves { get; set; }
 
         /// <summary>
         /// Gets or sets the number of Withdraws.
