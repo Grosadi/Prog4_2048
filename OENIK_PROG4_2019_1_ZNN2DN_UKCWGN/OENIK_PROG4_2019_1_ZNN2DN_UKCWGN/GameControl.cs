518baa3a715228cf4bc5f410bb86bac26bdd71f8
grosadi <groska.adam98@gmail.com>
2020-04-13 19:26:46 +0200

    GameControl class added

---
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/GameControl.cs
@@ -0,0 +1,18 @@
+﻿using _2048.Logic;
+using _2048.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
+{
+    public class GameControl : FrameworkElement
+    {
+        GameLogic logic;
+        GameModel model;
+        GameRenderer renderer;
+    }
+}
