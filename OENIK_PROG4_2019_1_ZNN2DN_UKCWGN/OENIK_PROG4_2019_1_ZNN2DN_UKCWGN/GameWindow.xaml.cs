30e459c2e4601f476be9206c7c13560deed1992d
grosadi <groska.adam98@gmail.com>
2020-05-01 19:42:00 +0200

    working on GameWindow

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/GameWindow.xaml.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/GameWindow.xaml.cs
@@ -15,13 +15,46 @@ using System.Windows.Shapes;
 namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
 {
     /// <summary>
-    /// Interaction logic for GameWindow.xaml
+    /// Interaction logic for GameWindow.xaml.
     /// </summary>
     public partial class GameWindow : Window
     {
+        /// <summary>
+        /// Gets or sets the size of the table.
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the timer.
+        /// </summary>
+        public int Time { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameWindow"/> class.
+        /// Constructor for Load Game.
+        /// </summary>
         public GameWindow()
         {
-            InitializeComponent();
+            this.Size = 0;
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameWindow"/> class.
+        /// Constructor to start a new game.
+        /// </summary>
+        /// <param name="size">Size of the table.</param>
+        /// <param name="time">Time, if 0, the game is endless.</param>
+        public GameWindow(int size, int time)
+            : this()
+        {
+            this.Size = size;
+            this.Time = time;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
