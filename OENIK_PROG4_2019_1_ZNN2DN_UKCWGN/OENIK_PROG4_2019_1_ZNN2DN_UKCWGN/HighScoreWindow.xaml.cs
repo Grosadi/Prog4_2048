a052f1e89fe6753cf791e20b76b3462866dafe4b
grosadi <groska.adam98@gmail.com>
2020-05-01 19:39:31 +0200

    HighScoreWindow added

---
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/HighScoreWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
+{
+    /// <summary>
+    /// Interaction logic for HighScoreWindow.xaml
+    /// </summary>
+    public partial class HighScoreWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighScoreWindow"/> class.
+        /// </summary>
+        public HighScoreWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
