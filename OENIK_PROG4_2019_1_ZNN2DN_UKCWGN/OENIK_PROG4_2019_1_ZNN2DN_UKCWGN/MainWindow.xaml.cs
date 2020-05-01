30281b2582e1fc0b041d90008320cb5d194ae2ca
grosadi <groska.adam98@gmail.com>
2020-05-01 19:40:55 +0200

    MainWindow almost completed.

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/MainWindow.xaml.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow((int)this.size.Value, (int)this.time.Value);
+            gw.Show();
+            this.Close();
+        }
+
+        private void LoadGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow();
+            gw.Show();
+            this.Close();
+        }
+
+        private void HighScoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            HighScoreWindow hw = new HighScoreWindow();
+            hw.Show();
         }
     }
 }
