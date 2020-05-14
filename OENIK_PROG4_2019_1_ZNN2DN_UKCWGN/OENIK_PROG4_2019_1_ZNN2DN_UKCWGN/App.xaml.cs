c0610af7127c00dff9bb2962af03361934b77617
bogdanistvan <bogdanistvan0924@gmail.com>
2020-05-14 15:58:57 +0200

    inside changes

--- OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/App.xaml.cs
+++ OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/OENIK_PROG4_2019_1_ZNN2DN_UKCWGN/App.xaml.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// <copyright file="App.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace OENIK_PROG4_2019_1_ZNN2DN_UKCWGN
 {
+    using System.Windows;
+
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml.
     //// </summary>
     public partial class App : Application
     {
